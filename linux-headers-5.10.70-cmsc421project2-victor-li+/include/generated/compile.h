9be51dc937936c9e432f775871739b00339d4aa8
Victor-Li-1473 <vli1@umbc.edu>
2022-03-21 19:06:19 -0400

    first commit

---
+++ linux-headers-5.10.70-cmsc421project2-victor-li+/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Thu Feb 3 17:13:49 EST 2022"
+#define LINUX_COMPILE_BY "student"
+#define LINUX_COMPILE_HOST "CMSC421Debian"
+#define LINUX_COMPILER "gcc (Debian 10.2.1-6) 10.2.1 20210110, GNU ld (GNU Binutils for Debian) 2.35.2"
---
+++ linux-headers-5.10.70-cmsc421project2-victor-li+/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "5.10.70-cmsc421project2-victor-li+"
