b3c96cd71e37a7846e305fce5aa1e6a882b1a07a
Victor-Li-1473 <vli1@umbc.edu>
2022-04-06 23:13:06 -0400

    Inital commit

--- buffer/test_user.c
+++ buffer/test_user.c
@@ -7,7 +7,15 @@
 
 int main(int argc, char *argv[])
 {	
+	// Test the buffer is initalized properly
 	init_buffer_421();
+	print_buffer_421();
+	
+	// Test whether the buffer inserts 20 values properly
+	// The printes result should be: Node 1: 1	Node 2: 2 ... Node 20:20
+	for (int i = 1; i <= 20; i++){
+		insert_buffer_421(i);
+	}
 	print_buffer_421();
 	return 0;
 }
